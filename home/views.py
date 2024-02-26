0e4e61d5a9d07a1c5a652e351227f48996b67edb
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2024-02-25 21:05:50 -0500

    Add 'home' template and update related settings
    The implementation involves setting up a 'home' template in the Django project, while also modifying the settings at the same time. More specifically, 'TEMPLATES_DIR' was defined in 'settings.py', and a routing to 'home/home.html' was created in 'views.py'.

--- home/views.py
+++ home/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
 
 def index(request):
-    return HttpResponse('Hello, World!')
+    return TemplateResponse(request, 'home/home.html')

efcb15b9a26611478e63ad3c4f24d796ad9a4456
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2024-02-25 21:56:10 -0500

    Update title in signup page HTML template
    The HTML title in the signup page template has been updated from a generic "Title" to a more specific "Signup". This improves clarity and SEO, as well as providing a better user experience.

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Signup
 
-# Register your models here.
+@admin.register(Signup)
+class SignupAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'created_at')
+    search_fields = ('email', 'name')
